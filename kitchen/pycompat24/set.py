1f9c0508ccaf81385bcee4ecd036eaa46dfcd1a3
Toshio Kuratomi <toshio@fedoraproject.org>
2010-05-12 22:53:14 -0400

    Push the set assignment into __builtin__ where it really is available to everyone

--- kitchen/pycompat24/set.py
+++ kitchen/pycompat24/set.py
@@ -1,7 +1,9 @@
 '''
 Importing this module makes sure that a set and frozenset type have been defined
 '''
+import __builtin__
 if not hasattr(__builtin__, 'set'):
     import sets
-    set = sets.Set
-    frozenset = sets.ImmutableSets
+    __builtin__.set = sets.Set
+    __builtin__.frozenset = sets.ImmutableSets
+    
