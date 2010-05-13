d8a3354bb5ca16ec6fbab80adf59192427d48a8a
Toshio Kuratomi <toshio@fedoraproject.org>
2010-05-12 22:43:05 -0400

    Merge methods from python-fedora in.

--- kitchen/text/encoding.py
+++ kitchen/text/encoding.py
@@ -12,6 +12,10 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
 
+'''
+Utility functions to handle conversion of byte strings and unicode strings.
+'''
+from kitchen import _
 
 
 # This is ported from ustr_utf8_* which I got from:
@@ -407,34 +411,169 @@ def utf8_text_fill(text, *args, **kwargs):
     return '\n'.join(utf8_text_wrap(text, *args, **kwargs))
 # ----------------------------- END utf8 -----------------------------
 
-def to_unicode(obj, encoding='utf-8', errors='replace'):
-    ''' convert a 'str' to 'unicode' '''
-    if isinstance(obj, basestring):
-        if not isinstance(obj, unicode):
-            obj = unicode(obj, encoding, errors)
-    return obj
+def to_unicode(obj, encoding='utf8', errors='replace', non_string='empty'):
+    '''Convert an object into a unicode string
 
-def to_utf8(obj, errors='replace'):
-    '''convert 'unicode' to an encoded utf-8 byte string '''
-    if isinstance(obj, unicode):
-        obj = obj.encode('utf-8', errors)
-    return obj
+    Usually, this should be used on a byte string but it can take byte strings
+    and unicode strings intelligently.  non_string objects are handled in
+    different ways depending on the setting of the non_string parameter.
+
+    The default values of this function are set so as to always return
+    a unicode string and never raise an error when converting from bytes to
+    unicode.  However, when you do not pass validly encoded text as the byte
+    string (or a non-string object), you may end up with output that you don't
+    expect.  Be sure you understand the requirements of your data, not just
+    ignore errors by passing it through this function.
+
+    :arg obj: Object to convert to a unicode string.  This should normally be
+        a byte string
+    :kwarg encoding: What encoding to try converting the byte string as.
+        Defaults to utf8
+    :kwarg errors: If errors are given, perform this action.  Defaults to
+        'replace' which replaces the error with a character that means the bytes
+        were unable to be decoded.  Other values are those that can be given to
+        the unicode constructor, for instance 'strict' which raises an
+        exception and 'ignore' which simply omits the non-decodable
+        characters.
+    :kwargs non_string: How to treat non_string values.  Possible values are:
+            :empty: Return an empty string
+            :strict: Raise a TypeError
+            :passthru: Return the object unchanged
+            :repr: Attempt to return a unicode string of the repr of the
+                object
+        The Default is 'empty'
+    :raises TypeError: if non_string is strict and a non-basestring object is
+        passed in or if :param:`non_string` is set to an unknown value
+    :returns: unicode string or the original object depending on the value of
+        non_string.
+    '''
+    if isinstance(obj, basestring):
+        if isinstance(obj, unicode):
+            return obj
+        return unicode(obj, encoding=encoding, errors=errors)
+    if non_string == 'empty':
+        return u''
+    elif non_string == 'passthru':
+        return obj
+    elif non_string in ('repr', 'strict'):
+        obj_repr = repr(obj)
+        if not isinstance(obj_repr, unicode):
+            unicode(obj_repr, encoding=encoding, errors=errors)
+        if non_string == 'strict':
+            return obj_repr
+        raise TypeError(_('to_unicode was given "%(obj)s" which is neither'
+            ' a byte string (str) or a unicode string') % {'obj': obj_repr})
+
+    raise TypeError(_('non_string value, %(param)s, is not set to a valid'
+        ' action') % {'param': non_string})
+
+def to_bytes(obj, encoding='utf8', errors='replace', non_string='empty'):
+    '''Convert an object into a byte string
+
+    Usually, this should be used on a unicode string but it can take byte
+    strigs and unicode strings intelligently.  non_string objects are handled
+    in different ways depending on the setting of the non_string parameter.
+
+    The default values of this function are set so as to always return
+    a byte string and never raise an error when converting from unicode to
+    bytes.  However, when you do not pass an encoding that can validly encode
+    the object (or a non-string object), you may end up with output that you
+    don't expect.  Be sure you understand the requirements of your data, not
+    just ignore errors by passing it through this function.
+
+    :arg obj: Object to convert to a byte string.  this should normally be
+        a unicode string.
+    :kwarg encoding: What encoding to try using to convert the unicode string
+        into bytes.  Defaults to utf8
+    :kwarg errors: If errors are given, perform this action.  Defaults to
+        'replace' which replaces the error with a '?' character to show
+        a character was wunable to be decoded.  Other values are those that
+        can be given to the :function:`str.encode()` method.  For instance
+        'strict' which raises an exception and 'ignore' which simply omits the
+        non-encodable characters.
+    :kwargs non_string: How to treat non_string values.  Possible values are:
+            :empty: Return an empty byte string
+            :strict: Raise a TypeError
+            :passthru: Return the object unchanged
+            :repr: Attempt to return a byte string of the repr of the
+                object
+        The Default is 'empty'
+    :raises TypeError: if non_string is strict and a non-basestring object is
+        passed in or if :param:`non_string` is set to an unknown value
+    :returns: byte string or the original object depending on the value of
+        non_string.
+    '''
+    if isinstance(obj, basestring):
+        if isinstance(obj, str):
+            return obj
+        return obj.encode(encoding, errors)
+    if non_string == 'empty':
+        return ''
+    elif non_string == 'passthru':
+        return obj
+    elif non_string in ('repr', 'strict'):
+        obj_repr = repr(obj)
+        if isinstance(obj_repr, unicode):
+            obj_repr =  obj_repr.encode(encoding, errors)
+        else:
+            obj_repr = str(obj_repr)
+        if non_string == 'repr':
+            return obj_repr
+        raise TypeError(_('to_bytes was given "%(obj)s" which is neither'
+            ' a unicode string or a byte string (str)') % {'obj': obj_repr})
+
+    raise TypeError(_('non_string value, %(param)s, is not set to a valid'
+        ' action') % {'param': non_string})
+
+### skvidal: Will changing behaviour from passthru to empty cause issues for yum?
+#def to_unicode(obj, encoding='utf-8', errors='replace', non_string='passthru'):
+#    ''' convert a 'str' to 'unicode' '''
+#    if isinstance(obj, basestring):
+#        if not isinstance(obj, unicode):
+#            obj = unicode(obj, encoding, errors)
+#    return obj
+
+def to_utf8(obj, errors='replace', non_string='passthru'):
+    '''Compat with yum to_utf8 function.
+
+    convert 'unicode' to an encoded utf-8 byte string
+
+    deprecate: use to_bytes with appropriate arguments
+    '''
+    return to_bytes('utf8', errors='replace', non_string=non_string)
 
 # Don't use this, to_unicode should just work now
 def to_unicode_maybe(obj, encoding='utf-8', errors='replace'):
-    ''' Don't ask don't tell, only use when you must '''
-    try:
-        return to_unicode(obj, encoding, errors)
-    except UnicodeEncodeError:
-        return obj
+    ''' Don't ask don't tell, only use when you must
+    deprecated: just use to_unicode with appropriate arguments
 
-def to_str(obj):
-    """ Convert something to a string, if it isn't one. """
+    Convert a string to unicode without raising an error
+    '''
+    return to_unicode(obj, encoding, errors, non_string='passthru')
+
+def to_string(obj):
+    '''Convert something to a string, if it isn't one
+    
+    This may return either a unicode string or a byte string.  Its main
+    purpose is just to get a representation for an object.
+    '''
     # NOTE: unicode counts as a string just fine. We just want objects to call
     # their __str__ methods.
     if not isinstance(obj, basestring):
-        obj = str(obj)
+        try:
+            obj = str(obj)
+        except (UnicodeEncodeError, UnicodeDecodeError):
+            obj = repr(obj)
     return obj
+
+### str is also the constructor for byte strings so it's not a good name for
+### something that can also return unicode strings
+def to_str(obj):
+    '''Convert something to a string.
+    
+    deprecated: Use to_string to do the same thing
+    '''
+    return to_string(obj)
 
 def str_eq(a, b):
     """ convert between unicode and not and compare them, w/o warning or being annoying"""
@@ -443,6 +582,5 @@ def str_eq(a, b):
             return True
     elif to_utf8(a) == to_utf8(b):
         return True
-    
-    return False
 
+    return False
