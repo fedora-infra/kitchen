a986638cccf22f5406ed2217bdfa5ec43b2f85ae
Toshio Kuratomi <toshio@fedoraproject.org>
2010-05-12 22:55:45 -0400

    Add the LGPL header

--- kitchen/pycompat24/set.py
+++ kitchen/pycompat24/set.py
@@ -1,3 +1,25 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (c) 2010 Red Hat, Inc
+#
+# This file is part of kitchen.core
+# 
+# kitchen.core is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2.1 of the License, or (at your option) any later version.
+# 
+# kitchen.core is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+# 
+# You should have received a copy of the GNU Lesser General Public
+# License along with python-fedora; if not, see <http://www.gnu.org/licenses/>
+#
+# Authors:
+#   Toshio Kuratomi <toshio@fedoraproject.org>
+
 '''
 Importing this module makes sure that a set and frozenset type have been defined
 '''
