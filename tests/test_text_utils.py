be7fa1bd8c3103827676c5c8a7b0426dc5286447
Toshio Kuratomi <toshio@fedoraproject.org>
2010-05-18 22:28:59 -0400

    Finish tests for text.utils

--- tests/test_text_utils.py
+++ tests/test_text_utils.py
@@ -4,6 +4,7 @@ import unittest
 from nose import tools
 
 from kitchen.text import utils
+from kitchen.text.exceptions import ControlCharError
 from kitchen.text.converters import to_unicode
 
 class TestTextUtils(unittest.TestCase):
@@ -14,13 +15,18 @@ class TestTextUtils(unittest.TestCase):
     utf8_japanese = u_japanese.encode('utf8')
     euc_jp_japanese = u_japanese.encode('euc_jp')
 
-    def setUp(self):
-        pass
+    u_ascii_chars = u' '.join(map(unichr, range(0, 256)))
+    u_ascii_no_ctrl = u''.join([c for c in u_ascii_chars if ord(c) not in utils._control_codes])
+    u_ascii_ctrl_replace = u_ascii_chars.translate(dict([(c, u'?') for c in utils._control_codes]))
+    utf8_ascii_chars = u_ascii_chars.encode('utf8')
 
-    def tearDown(self):
-        pass
+    u_entity_escape = u'Test: &lt;&quot;&amp;&quot;&gt; &ndash; ' + u_japanese.encode('ascii', 'xmlcharrefreplace') + '&#xe9;'
+    u_entity = u'Test: <"&"> – ' + u_japanese + u'é'
 
     def test_guess_encoding_no_chardet(self):
+        # Test that unicode strings are not allowed
+        tools.assert_raises(TypeError, utils.guess_encoding, self.u_spanish)
+
         tools.ok_(utils.guess_encoding(self.utf8_spanish, disable_chardet=True) == 'utf8')
         tools.ok_(utils.guess_encoding(self.latin1_spanish, disable_chardet=True) == 'latin1')
         tools.ok_(utils.guess_encoding(self.utf8_japanese, disable_chardet=True) == 'utf8')
@@ -45,3 +51,27 @@ class TestTextUtils(unittest.TestCase):
         tools.ok_(utils.str_eq(self.euc_jp_japanese, self.euc_jp_japanese) == True)
         tools.ok_(utils.str_eq(self.u_japanese, self.euc_jp_japanese) == False)
         tools.ok_(utils.str_eq(self.u_japanese, self.euc_jp_japanese, encoding='euc_jp') == True)
+
+    def test_process_control_chars(self):
+        tools.assert_raises(TypeError, utils.process_control_chars, 'byte string')
+        tools.assert_raises(ControlCharError, utils.process_control_chars,
+                *[self.u_ascii_chars], **{'strategy':'strict'})
+        tools.ok_(utils.process_control_chars(self.u_ascii_chars,
+            strategy='ignore') == self.u_ascii_no_ctrl)
+        tools.ok_(utils.process_control_chars(self.u_ascii_chars,
+            strategy='replace') == self.u_ascii_ctrl_replace)
+
+    def test_html_entities_unescape(self):
+        tools.assert_raises(TypeError, utils.html_entities_unescape, 'byte string')
+        tools.ok_(utils.html_entities_unescape(self.u_entity_escape) == self.u_entity)
+
+    def test_byte_string_valid_xml(self):
+        tools.assert_false(utils.byte_string_valid_xml(u'unicode string'))
+
+        tools.assert_true(utils.byte_string_valid_xml(self.utf8_japanese))
+        tools.assert_true(utils.byte_string_valid_xml(self.euc_jp_japanese, 'euc_jp'))
+
+        tools.assert_false(utils.byte_string_valid_xml(self.utf8_japanese, 'euc_jp'))
+        tools.assert_false(utils.byte_string_valid_xml(self.euc_jp_japanese, 'utf8'))
+
+        tools.assert_false(utils.byte_string_valid_xml(self.utf8_ascii_chars))
