08c0fcb0a5801480271f74606bcb24c17acd377c
Toshio Kuratomi <toshio@fedoraproject.org>
2010-05-20 16:05:56 -0400

    Implement the base64 modern functions

---
+++ kitchen/pycompat24/base64modern.py
@@ -0,0 +1,111 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (c) 2010 Red Hat, Inc
+#
+# This file is part of kitchen.core
+# 
+# kitchen.core is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2.1 of the License, or (at your option) any later version.
+# 
+# kitchen.core is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+# 
+# You should have received a copy of the GNU Lesser General Public
+# License along with python-fedora; if not, see <http://www.gnu.org/licenses/>
+#
+# Authors:
+#   Toshio Kuratomi <toshio@fedoraproject.org>
+
+'''
+Implement the modern base64 interface.  Note that this only implements the
+base64 functions.  Specifically, it does not implement b32encode, b32decode,
+b16encode, or b16decode.
+'''
+import base64
+
+def _b64encode(s, altchars=None):
+    '''Compatibility function for python-2.4's base64.b64encode
+
+    .. seealso:: http://docs.python.org/library/base64.html
+    '''
+    s = base64.encodestring(s)
+    if altchars:
+        for chars in zip(('+','/'), altchars):
+            s = s.replace(chars[0], chars[1])
+    return ''.join(s.split())
+
+def _b64decode(s, altchars=None):
+    '''Compatibility function for python-2.4's base64.b64decode
+
+    .. seealso:: http://docs.python.org/library/base64.html
+    '''
+    if altchars:
+        for chars in zip(altchars, ('+', '/')):
+            s = s.replace(chars[0], chars[1])
+    s = base64.decodestring(s)
+    return s
+
+def _standard_b64encode(s):
+    '''Compatibility function for python-2.4's base64.standard_b64encode
+
+    .. seealso:: http://docs.python.org/library/base64.html
+    '''
+    return b64encode(s)
+
+def _standard_b64decode(s):
+    '''Compatibility function for python-2.4's base64.standard_b64decode
+
+    .. seealso:: http://docs.python.org/library/base64.html
+    '''
+    return b64decode(s)
+
+def _urlsafe_b64encode(s):
+    '''Compatibility function for python-2.4's base64.urlsafe_b64encode
+
+    .. seealso:: http://docs.python.org/library/base64.html
+    '''
+    return b64encode(s, altchars='-_')
+
+def _urlsafe_b64decode(s):
+    '''Compatibility function for python-2.4's base64.urlsafe_b64decode
+
+    .. seealso:: http://docs.python.org/library/base64.html
+    '''
+    return b64decode(s, altchars='-_')
+
+if hasattr(base64, 'b64encode'):
+    b64encode = base64.b64encode
+else:
+    b64encode = _b64encode
+
+if hasattr(base64, 'b64decode'):
+    b64decode = base64.b64decode
+else:
+    b64decode = _b64decode
+
+if hasattr(base64, 'standard_b64encode'):
+    standard_b64encode = base64.standard_b64encode
+else:
+    standard_b64encode = _standard_b64encode
+
+if hasattr(base64, 'standard_b64decode'):
+    standard_b64decode = base64.standard_b64decode
+else:
+    standard_b64decode = _standard_b64decode
+
+if hasattr(base64, 'urlsafe_b64encode'):
+    urlsafe_b64encode = base64.urlsafe_b64encode
+else:
+    urlsafe_b64encode = _urlsafe_b64encode
+
+if hasattr(base64, 'urlsafe_b64decode'):
+    urlsafe_b64decode = base64.urlsafe_b64decode
+else:
+    urlsafe_b64decode = _urlsafe_b64decode
+
+__all__ = ( 'b64decode', 'b64encode', 'standard_b64decode',
+        'standard_b64encode', 'urlsafe_b64decode', 'urlsafe_b64encode',)
--- tests/test_pycompat24.py
+++ tests/test_pycompat24.py
@@ -2,11 +2,14 @@
 #
 import unittest
 from nose import tools
+from nose.plugins.skip import SkipTest
 
 import __builtin__
+import base64
 import sets
 
 from kitchen.pycompat24 import builtinset
+from kitchen.pycompat24 import base64modern
 
 class TestSetsNoOverwrite(unittest.TestCase):
     def setUp(self):
@@ -66,4 +69,34 @@ class TestSubprocess(unittest.TestCase):
     pass
 
 class TestBase64(unittest.TestCase):
-    pass
+    b_byte_chars = ' '.join(map(chr, range(0, 256)))
+    b_byte_encoded = 'ACABIAIgAyAEIAUgBiAHIAggCSAKIAsgDCANIA4gDyAQIBEgEiATIBQgFSAWIBcgGCAZIBogGyAcIB0gHiAfICAgISAiICMgJCAlICYgJyAoICkgKiArICwgLSAuIC8gMCAxIDIgMyA0IDUgNiA3IDggOSA6IDsgPCA9ID4gPyBAIEEgQiBDIEQgRSBGIEcgSCBJIEogSyBMIE0gTiBPIFAgUSBSIFMgVCBVIFYgVyBYIFkgWiBbIFwgXSBeIF8gYCBhIGIgYyBkIGUgZiBnIGggaSBqIGsgbCBtIG4gbyBwIHEgciBzIHQgdSB2IHcgeCB5IHogeyB8IH0gfiB/IIAggSCCIIMghCCFIIYghyCIIIkgiiCLIIwgjSCOII8gkCCRIJIgkyCUIJUgliCXIJggmSCaIJsgnCCdIJ4gnyCgIKEgoiCjIKQgpSCmIKcgqCCpIKogqyCsIK0griCvILAgsSCyILMgtCC1ILYgtyC4ILkguiC7ILwgvSC+IL8gwCDBIMIgwyDEIMUgxiDHIMggySDKIMsgzCDNIM4gzyDQINEg0iDTINQg1SDWINcg2CDZINog2yDcIN0g3iDfIOAg4SDiIOMg5CDlIOYg5yDoIOkg6iDrIOwg7SDuIO8g8CDxIPIg8yD0IPUg9iD3IPgg+SD6IPsg/CD9IP4g/w=='
+    b_byte_encoded_urlsafe = 'ACABIAIgAyAEIAUgBiAHIAggCSAKIAsgDCANIA4gDyAQIBEgEiATIBQgFSAWIBcgGCAZIBogGyAcIB0gHiAfICAgISAiICMgJCAlICYgJyAoICkgKiArICwgLSAuIC8gMCAxIDIgMyA0IDUgNiA3IDggOSA6IDsgPCA9ID4gPyBAIEEgQiBDIEQgRSBGIEcgSCBJIEogSyBMIE0gTiBPIFAgUSBSIFMgVCBVIFYgVyBYIFkgWiBbIFwgXSBeIF8gYCBhIGIgYyBkIGUgZiBnIGggaSBqIGsgbCBtIG4gbyBwIHEgciBzIHQgdSB2IHcgeCB5IHogeyB8IH0gfiB_IIAggSCCIIMghCCFIIYghyCIIIkgiiCLIIwgjSCOII8gkCCRIJIgkyCUIJUgliCXIJggmSCaIJsgnCCdIJ4gnyCgIKEgoiCjIKQgpSCmIKcgqCCpIKogqyCsIK0griCvILAgsSCyILMgtCC1ILYgtyC4ILkguiC7ILwgvSC-IL8gwCDBIMIgwyDEIMUgxiDHIMggySDKIMsgzCDNIM4gzyDQINEg0iDTINQg1SDWINcg2CDZINog2yDcIN0g3iDfIOAg4SDiIOMg5CDlIOYg5yDoIOkg6iDrIOwg7SDuIO8g8CDxIPIg8yD0IPUg9iD3IPgg-SD6IPsg_CD9IP4g_w=='
+
+    def test_base64modern_encode(self):
+        tools.ok_(base64modern._b64encode(self.b_byte_chars) == self.b_byte_encoded)
+        tools.ok_(base64modern._b64encode(self.b_byte_chars, altchars='-_') == self.b_byte_encoded_urlsafe)
+        tools.ok_(base64modern._standard_b64encode(self.b_byte_chars) == self.b_byte_encoded)
+        tools.ok_(base64modern._urlsafe_b64encode(self.b_byte_chars) == self.b_byte_encoded_urlsafe)
+
+        tools.ok_(base64modern.b64encode(self.b_byte_chars) == self.b_byte_encoded)
+        tools.ok_(base64modern.b64encode(self.b_byte_chars, altchars='-_') == self.b_byte_encoded_urlsafe)
+        tools.ok_(base64modern.standard_b64encode(self.b_byte_chars) == self.b_byte_encoded)
+        tools.ok_(base64modern.urlsafe_b64encode(self.b_byte_chars) == self.b_byte_encoded_urlsafe)
+
+    def test_base64modern_decode(self):
+        tools.ok_(base64modern._b64decode(self.b_byte_encoded) == self.b_byte_chars)
+        tools.ok_(base64modern._b64decode(self.b_byte_encoded_urlsafe, altchars='-_') == self.b_byte_chars)
+        tools.ok_(base64modern._standard_b64decode(self.b_byte_encoded) == self.b_byte_chars)
+        tools.ok_(base64modern._urlsafe_b64decode(self.b_byte_encoded_urlsafe) == self.b_byte_chars)
+
+        tools.ok_(base64modern.b64decode(self.b_byte_encoded) == self.b_byte_chars)
+        tools.ok_(base64modern.b64decode(self.b_byte_encoded_urlsafe, altchars='-_') == self.b_byte_chars)
+        tools.ok_(base64modern.standard_b64decode(self.b_byte_encoded) == self.b_byte_chars)
+        tools.ok_(base64modern.urlsafe_b64decode(self.b_byte_encoded_urlsafe) == self.b_byte_chars)
+
+    def test_base64modern_stdlib_compat(self):
+        if not hasattr(base64, 'b64encode'):
+            raise SkipTest('Python-2.3 doesn\'t have b64encode to compare against')
+        tools.ok_(base64modern._b64encode(self.b_byte_chars) == base64.b64encode(self.b_byte_chars))
+        tools.ok_(base64modern._b64decode(self.b_byte_chars) == base64.b64decode(self.b_byte_chars))
