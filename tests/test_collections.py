56a546b8d026032ea2ab5469940141ad8b41bf2d
Toshio Kuratomi <toshio@fedoraproject.org>
2010-05-25 23:25:32 -0400

    Changes to test cases so they run on python-2.3

--- tests/test_collections.py
+++ tests/test_collections.py
@@ -3,6 +3,9 @@
 import unittest
 from nose import tools
 
+from kitchen.pycompat24.builtinset import add_builtin_set
+add_builtin_set()
+
 from kitchen.collections.strictdict import StrictDict
 
 def test_strict_dict_get_set():
@@ -34,38 +37,75 @@ class TestStrictDict(unittest.TestCase):
     def tearDown(self):
         del(self.d)
 
-    def _compare_lists(self, list1, list2):
-        '''because we have a mixture of bytes and unicode we have to compare these lists manually and inefficiently'''
+    def _compare_lists(self, list1, list2, debug=False):
+        '''We have a mixture of bytes and unicode and need python2.3 compat
+
+        So we have to compare these lists manually and inefficiently
+        '''
+        def _compare_lists_helper(compare_to, dupes, idx, length):
+            if i not in compare_to:
+                return False
+            for n in range(1, length + 1):
+                if i not in dupes[n][idx]:
+                    dupes[n][idx].add(i)
+                    return True
         if len(list1) != len(list2):
             return False
 
-        list1_dupes = dict([(i, []) for i in range(1, len(list1)+1)])
-        list2_dupes = dict([(i, []) for i in range(1, len(list1)+1)])
+        list1_dupes = dict([(i, (set(), set(), set())) for i in range(1, len(list1)+1)])
+        list2_dupes = dict([(i, (set(), set(), set())) for i in range(1, len(list1)+1)])
 
+        list1_u = [l for l in list1 if isinstance(l, unicode)]
+        list1_b = [l for l in list1 if isinstance(l, str)]
+        list1_o = [l for l in list1 if not (isinstance(l, unicode) or isinstance(l, str))]
+
+        list2_u = [l for l in list2 if isinstance(l, unicode)]
+        list2_b = [l for l in list2 if isinstance(l, str)]
+        list2_o = [l for l in list2 if not (isinstance(l, unicode) or isinstance(l, str))]
 
         for i in list1:
-            if i not in list2:
-                return False
-            for n in range(1, len(list1) + 1):
-                if i not in list1_dupes[n]:
-                    list1_dupes[n].append(i)
-                    break
-
-        if list1_dupes[1]:
-            for i in list2:
-                if i not in list1:
+            if isinstance(i, unicode):
+                if not _compare_lists_helper(list2_u, list1_dupes, 0, len(list1)):
                     return False
-                for n in range(1, len(list1) +1):
-                    if i not in list2_dupes[n]:
-                        list2_dupes[n].append(i)
-                        break
+            elif isinstance(i, str):
+                if not _compare_lists_helper(list2_b, list1_dupes, 1, len(list1)):
+                    return False
+            else:
+                if not _compare_lists_helper(list2_o, list1_dupes, 2, len(list1)):
+                    return False
 
-            for i in range(1, len(list1)+1):
-                for n in list1_dupes:
-                    if n not in list2_dupes:
+        if list1_dupes[2][0] or list1_dupes[2][1] or list1_dupes[2][2]:
+            for i in list2:
+                if isinstance(i, unicode):
+                    if not _compare_lists_helper(list1_u, list2_dupes, 0, len(list1)):
+                        return False
+                elif isinstance(i, str):
+                    if not _compare_lists_helper(list1_b, list2_dupes, 1, len(list1)):
+                        return False
+                else:
+                    if not _compare_lists_helper(list1_o, list2_dupes, 2, len(list1)):
+                        return False
+
+            for i in range(2, len(list1)+1):
+                for n in list1_dupes[i]:
+                    if n not in list2_dupes[i]:
                         return False
 
         return True
+
+    def test__compare_list(self):
+        '''*sigh* this test support function is so complex we need to test it'''
+        tools.ok_(self._compare_lists(['a', 'b', 'c'], ['c', 'a', 'b']))
+        tools.ok_(not self._compare_lists(['b', 'c'], ['c', 'a', 'b']))
+        tools.ok_(not self._compare_lists([u'a', 'b'], ['a', 'b']))
+        tools.ok_(not self._compare_lists(['a', u'b'], [u'a', 'b']))
+        tools.ok_(self._compare_lists(['a', 'b', 1], ['a', 1, 'b']))
+        tools.ok_(self._compare_lists([u'a', u'b'], [u'a', u'b']))
+        tools.ok_(self._compare_lists([u'a', 'b'], [u'a', 'b']))
+        tools.ok_(not self._compare_lists([u'a', 'b'], [u'a', u'b']))
+        tools.ok_(self._compare_lists([u'a', 'b', 'b', 'c', u'a'], [u'a', u'a', 'b', 'c', 'b']))
+        tools.ok_(not self._compare_lists([u'a', 'b', 'b', 'c', 'a'], [u'a', u'a', 'b', 'c', 'b']))
+        tools.ok_(not self._compare_lists([u'a', 'b', 'b', 'c', u'a'], [u'a', 'b', 'b', 'c', 'b']))
 
     def test_strict_dict_len(self):
         '''StrictDict len'''
@@ -100,17 +140,17 @@ class TestStrictDict(unittest.TestCase):
 
     def test_strict_dict_contains(self):
         '''StrictDict contains function'''
-        tools.assert_false('b' in self.d)
-        tools.assert_false(u'b' in self.d)
+        tools.ok_('b' not in self.d)
+        tools.ok_(u'b' not in self.d)
         tools.ok_('\xf1' in self.d)
         tools.ok_(u'\xf1' in self.d)
         tools.ok_('a' in self.d)
         tools.ok_(u'a' in self.d)
 
         del(self.d[u'\xf1'])
-        tools.assert_false(u'\xf1' in self.d)
+        tools.ok_(u'\xf1' not in self.d)
         tools.ok_('\xf1' in self.d)
 
         del(self.d['a'])
         tools.ok_(u'a' in self.d)
-        tools.assert_false('a' in self.d)
+        tools.ok_('a' not in self.d)
