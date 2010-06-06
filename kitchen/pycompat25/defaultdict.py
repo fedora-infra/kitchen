33daedd565dd6cf26fec4a627041393057f5014b
Toshio Kuratomi <toshio@fedoraproject.org>
2010-06-06 14:00:37 -0400

    Found that the license for this code is actually python license.  Update header.

--- kitchen/pycompat25/defaultdict.py
+++ kitchen/pycompat25/defaultdict.py
@@ -3,27 +3,61 @@
 # by Jef Spaleta This code provides an emulation for the defaultdict
 # functionality introduced in python 2.5's collection module
 #
+# Changes from the original:
+# * Change the return value from __reduce__ to use iteritems() to prevent
+#   a segfault when pickling. (Jef Spaleta)
+# * Change how we setup the module to use collections.defaultdict by default
+#   (Toshio Kuratomi)
+#
 # Copyright (c) 2007 Justin Kirtland
 # 
-#
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"), to
-# deal in the Software without restriction, including without limitation the
-# rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
-# sell copies of the Software, and to permit persons to whom the Software is
-# furnished to do so, subject to the following conditions:
-#
-# The above copyright notice and this permission notice shall be included in
-# all copies or substantial portions of the Software.
-#
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
-# FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
-# IN THE SOFTWARE.
-#
+# PYTHON SOFTWARE FOUNDATION LICENSE VERSION 2
+# --------------------------------------------
+# 
+# 1. This LICENSE AGREEMENT is between the Python Software Foundation ("PSF"),
+#    and the Individual or Organization ("Licensee") accessing and otherwise
+#    using this software ("Python") in source or binary form and its
+#    associated documentation.
+# 
+# 2. Subject to the terms and conditions of this License Agreement, PSF hereby
+#    grants Licensee a nonexclusive, royalty-free, world-wide license to
+#    reproduce, analyze, test, perform and/or display publicly, prepare
+#    derivative works, distribute, and otherwise use Python alone or in any
+#    derivative version, provided, however, that PSF's License Agreement and
+#    PSF's notice of copyright, i.e., "Copyright (c) 2001, 2002, 2003, 2004,
+#    2005, 2006 Python Software Foundation; All Rights Reserved" are retained
+#    in Python alone or in any derivative version prepared by Licensee.
+# 
+# 3. In the event Licensee prepares a derivative work that is based on or
+#    incorporates Python or any part thereof, and wants to make the derivative
+#    work available to others as provided herein, then Licensee hereby agrees
+#    to include in any such work a brief summary of the changes made to
+#    Python.
+# 
+# 4. PSF is making Python available to Licensee on an "AS IS" basis.  PSF
+#    MAKES NO REPRESENTATIONS OR WARRANTIES, EXPRESS OR IMPLIED.  BY WAY OF
+#    EXAMPLE, BUT NOT LIMITATION, PSF MAKES NO AND DISCLAIMS ANY
+#    REPRESENTATION OR WARRANTY OF MERCHANTABILITY OR FITNESS FOR ANY
+#    PARTICULAR PURPOSE OR THAT THE USE OF PYTHON WILL NOT INFRINGE ANY THIRD
+#    PARTY RIGHTS.
+# 
+# 5. PSF SHALL NOT BE LIABLE TO LICENSEE OR ANY OTHER USERS OF PYTHON FOR ANY
+#    INCIDENTAL, SPECIAL, OR CONSEQUENTIAL DAMAGES OR LOSS AS A RESULT OF
+#    MODIFYING, DISTRIBUTING, OR OTHERWISE USING PYTHON, OR ANY DERIVATIVE
+#    THEREOF, EVEN IF ADVISED OF THE POSSIBILITY THEREOF.
+# 
+# 6. This License Agreement will automatically terminate upon a material
+#    breach of its terms and conditions.
+# 
+# 7. Nothing in this License Agreement shall be deemed to create any
+#    relationship of agency, partnership, or joint venture between PSF and
+#    Licensee.  This License Agreement does not grant permission to use PSF
+#    trademarks or trade name in a trademark sense to endorse or promote
+#    products or services of Licensee, or any third party.
+# 
+# 8. By copying, installing or otherwise using Python, Licensee agrees to be
+#    bound by the terms and conditions of this License Agreement.
+
 '''
 -----------
 defaultdict
@@ -32,22 +66,27 @@ defaultdict
 This is a pure python implementation of defaultdict that is compatible with
 the defaultdict class provided by python-2.5 and above.
 
-.. seealso:: :class:`collections.defaultdict` for documentation on this module
-
+.. seealso::
+    :class:`collections.defaultdict`
+        for documentation on this module
 '''
+
+# Pylint disabled messages
+#
+# :C0103: We're defnining a compatible class name therefore we need to match
+#   the format of that name.
+
 import types
 
 from kitchen import _
 
 try:
-    # :W0611: We're importing the 2.5 version first.  If that doesn't work,
-    # implement our own.
-    # pylint: disable-msg=W0611
     from collections import defaultdict
 except ImportError:
+    # pylint:disable-msg=C0103
     defaultdict = None
 
-class _defaultdict(dict):
+class _DefaultDict(dict):
     def __init__(self, default_factory=None, *args, **kwargs):
         if (default_factory is not None and
             not hasattr(default_factory, '__call__')):
@@ -89,13 +128,14 @@ class _defaultdict(dict):
         # recursion
         if isinstance(self.default_factory, types.MethodType) \
                 and self.default_factory.im_self is not None \
-                and issubclass(self.default_factory.im_class, _defaultdict):
+                and issubclass(self.default_factory.im_class, _DefaultDict):
             defrepr = '<bound method sub._factory of defaultdict(...'
         else:
             defrepr = repr(self.default_factory)
         return 'defaultdict(%s, %s)' % (defrepr, dict.__repr__(self))
 
 if not defaultdict:
-    defaultdict = _defaultdict
+    # pylint:disable-msg=C0103
+    defaultdict = _DefaultDict
 
 __all__ = ('defaultdict',)
