38965662f9ce06b1746487c4c56873adb241af7f
Toshio Kuratomi <toshio@fedoraproject.org>
2010-06-22 18:32:00 -0400

    Change subprocess so that we only import the bundled version if we don't have a recent enough version on the system.

--- kitchen/pycompat27/__init__.py
+++ kitchen/pycompat27/__init__.py
@@ -1,5 +1,11 @@
+import sys
+
 from kitchen.versioning import version_tuple_to_string
 
 __version_info__ = ((1, 0, 0),)
 __version__ = version_tuple_to_string(__version_info__)
 
+if sys.version_info >= (2, 7):
+    import subprocess as subprocess
+else:
+    import _subprocess as subprocess
