343665675818482a0e7c4161166d146f5e6ed9fe
Toshio Kuratomi <toshio@fedoraproject.org>
2010-07-24 21:10:23 -0400

    Add utf8 functions to the deprecation test

--- tests/test_deprecation.py
+++ tests/test_deprecation.py
@@ -5,6 +5,7 @@ from nose import tools
 
 import warnings
 from kitchen.text import converters
+from kitchen.text import utf8
 
 class TestDeprecated(unittest.TestCase):
     def setUp(self):
@@ -18,3 +19,11 @@ class TestDeprecated(unittest.TestCase):
         tools.assert_raises(DeprecationWarning, converters.to_utf8, u'café')
         tools.assert_raises(DeprecationWarning, converters.to_str, 5)
         tools.assert_raises(DeprecationWarning, converters.to_xml, 'test')
+
+        tools.assert_raises(DeprecationWarning, utf8.utf8_valid, 'test')
+        tools.assert_raises(DeprecationWarning, utf8.utf8_width, 'test')
+        tools.assert_raises(DeprecationWarning, utf8.utf8_width_chop, 'test')
+        tools.assert_raises(DeprecationWarning, utf8.utf8_width_fill, 'test', 'asd')
+        tools.assert_raises(DeprecationWarning, utf8.utf8_text_wrap, 'test')
+        tools.assert_raises(DeprecationWarning, utf8.utf8_text_fill, 'test')
+        tools.assert_raises(DeprecationWarning, utf8._utf8_width_le, 'test')
