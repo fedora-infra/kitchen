00dfde1bee9bbe74967f2f25d9cb8840067c4e45
Toshio Kuratomi <toshio@fedoraproject.org>
2010-09-13 14:20:48 -0400

    Fix problem with sync from upstream scrip prematurely exiting and add base64 as a file to sync

--- maintainers/sync-copied-files.py
+++ maintainers/sync-copied-files.py
@@ -6,17 +6,19 @@ import urlgrabber
 from hashlib import sha1
 
 files = {
-'kitchen/pycompat24/subprocess.py': 'http://svn.python.org/view/*checkout*/python/trunk/Lib/subprocess.py',
+'kitchen/pycompat27/_subprocess.py': 'http://svn.python.org/view/*checkout*/python/trunk/Lib/subprocess.py',
 'tests/test_subprocess.py': 'http://svn.python.org/view/*checkout*/python/trunk/Lib/test/test_subprocess.py',
 'tests/test_defaultdict.py': 'http://svn.python.org/view/*checkout*/python/trunk/Lib/test/test_defaultdict.py',
+'kitchen/pycompat24/base64/__init__.py': 'http://svn.python.org/view/*checkout*/python/trunk/Lib/base64.py',
+'tests/test_base64.py': 'http://svn.python.org/view/*checkout*/python/trunk/Lib/test/test_base64.py',
 }
-
 if __name__ == '__main__':
     os.chdir('sync')
 
     for filename in files:
         base_filename = os.path.basename(filename)
-        modname = os.path.splitext(base_filename)[0]
+        base_upstream_filename = os.path.basename(files[filename])
+        modname = os.path.splitext(base_upstream_filename)[0]
 
         if not os.path.exists(modname):
             os.mkdir(modname)
@@ -30,21 +32,24 @@ if __name__ == '__main__':
         os.chdir(modname)
 
         urlgrabber.urlgrab(files[filename])
-        sp = open(base_filename)
+        sp = open(base_upstream_filename)
         sp_sum = sha1(sp.read()).hexdigest()
         sp_sum_line = '%s\n' % sp_sum
 
         record = open('record', 'r')
         if sp_sum_line in record:
-            os.remove(base_filename)
-            sys.exit(0)
+            os.remove(base_upstream_filename)
+            os.chdir('..')
+            continue
 
         retrieved_at = int(time.time())
-        new_name = '%s-%s' % (retrieved_at, base_filename)
-        os.rename(base_filename, new_name)
+        new_name = '%s-%s' % (retrieved_at, base_upstream_filename)
+        os.rename(base_upstream_filename, new_name)
 
-        print 'New %s found: %s' % (base_filename, new_name)
+        print 'New %s found: %s' % (base_upstream_filename, new_name)
         record = open('record', 'a')
         record.writelines(sp_sum_line)
         record.close()
         os.chdir('..')
+
+    sys.exit(0)
