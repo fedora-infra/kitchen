47e1dbff5e5556c9223661d9711c43b0b7ec2dc1
Toshio Kuratomi <toshio@fedoraproject.org>
2010-09-17 14:51:07 -0400

    Docstring for kitchen.text

--- kitchen/text/__init__.py
+++ kitchen/text/__init__.py
@@ -1,3 +1,14 @@
+'''
+------------
+Kitchen.text
+------------
+
+Kitchen.text contains functions for manipulating text in python.
+
+This includes things like converting between byte strings and unicode,
+and displaying text on the screen.
+'''
+
 from kitchen.versioning import version_tuple_to_string
 
 __version_info__ = ((2, 0, 0),)
