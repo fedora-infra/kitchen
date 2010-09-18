205616b0a23143cdc5ceb6fb8333cf6074ce737b
Toshio Kuratomi <toshio@fedoraproject.org>
2010-09-17 20:25:46 -0400

    Fix pylint error in this module

--- kitchen/pycompat25/collections/__init__.py
+++ kitchen/pycompat25/collections/__init__.py
@@ -1,6 +1,8 @@
 try:
+    #:E0611: deafultdict doesn't exist in python-2.4 or less but that's why we
+    #   have it in a try: except:.  So we can use our version if necessary.
     from collections import defaultdict
 except ImportError:
-    from _defaultdict import defaultdict
+    from kitchen.pycompat25.collections._defaultdict import defaultdict
 
 __all__ = ('defaultdict',)
