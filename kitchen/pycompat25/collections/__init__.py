4c5b21359d066678d342c04c6f0d8f25029f200b
Toshio Kuratomi <toshio@fedoraproject.org>
2010-09-17 20:36:49 -0400

    Forgot to actually disable the pylint warning here

--- kitchen/pycompat25/collections/__init__.py
+++ kitchen/pycompat25/collections/__init__.py
@@ -1,6 +1,7 @@
 try:
     #:E0611: deafultdict doesn't exist in python-2.4 or less but that's why we
     #   have it in a try: except:.  So we can use our version if necessary.
+    #pylint:disable-msg=E0611
     from collections import defaultdict
 except ImportError:
     from kitchen.pycompat25.collections._defaultdict import defaultdict
