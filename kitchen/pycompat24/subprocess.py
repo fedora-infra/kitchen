cac8c000074d94ce7dd9a92e132ac33c6abb9961
Toshio Kuratomi <toshio@fedoraproject.org>
2010-09-17 20:40:39 -0400

    Disable pylint message and note that we're importing subprocess to have Only one copy for python-2.4 and python-2.7

--- kitchen/pycompat24/subprocess.py
+++ kitchen/pycompat24/subprocess.py
@@ -1,3 +1,5 @@
-# Rather than have two versions of subprocess, we import the python2.7 version
-# here as well
+# :W0611, W0401: Rather than have two versions of subprocess, we import the python2.7
+#   version here as well
+#pylint:disable-msg=W0611,W0401
 from kitchen.pycompat27.subprocess import *
+import kitchen.pycompat27.subprocess.__all__
