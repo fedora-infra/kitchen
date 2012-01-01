c5b1b1e762552eb63eb4e81ea22b9077e575d7bd
Toshio Kuratomi <toshio@fedoraproject.org>
2011-12-31 22:04:37 -0800

    Add an exclusion for another imported function in test__all__.py

--- tests/test__all__.py
+++ tests/test__all__.py
@@ -28,6 +28,7 @@ class Test__all__(object):
         ('kitchen', 'i18n', 'to_bytes'),
         ('kitchen', 'i18n', 'to_unicode'),
         ('kitchen', 'i18n', 'ENOENT'),
+        ('kitchen', 'i18n', 'byte_string_valid_encoding'),
         ('kitchen', 'iterutils', 'version_tuple_to_string'),
         ('kitchen', 'pycompat24', 'version_tuple_to_string'),
         ('kitchen', 'pycompat25', 'version_tuple_to_string'),
