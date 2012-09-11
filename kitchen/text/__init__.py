72a3094c652a2600ba52c54caaf25297c4c87897
Toshio Kuratomi <toshio@fedoraproject.org>
2012-09-10 23:51:42 -0700

    Update version -- we've now added API so we're bumping a different component of the version number

--- kitchen/text/__init__.py
+++ kitchen/text/__init__.py
@@ -11,7 +11,7 @@ and displaying text on the screen.
 
 from kitchen.versioning import version_tuple_to_string
 
-__version_info__ = ((2, 1, 2),)
+__version_info__ = ((2, 2, 0),)
 __version__ = version_tuple_to_string(__version_info__)
 
 __all__ = ('converters', 'exceptions', 'misc',)
